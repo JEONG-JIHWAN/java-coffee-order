6e9d551f7808075067fbb863800bcd246e8bb7b7
JEONG-JIWHAN <jjw981210@gmail.com>
2022-10-19 03:20:47 +0900

    feat: 메뉴 객체 생성

---
+++ src/main/java/coffee/order/Domain/Menu.java
@@ -0,0 +1,47 @@
+package coffee.order.Domain;
+
+import java.util.Objects;
+
+public class Menu {
+  private String id;
+  private String name;
+  private int price;
+  private Category category;
+
+  public Menu(String id, String name, int price, Category category) {
+    this.id = id;
+    this.name = name;
+    this.price = price;
+    this.category = category;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public String getName() {return name;}
+
+  public int getPrice() {
+    return price;
+  }
+  public Category getCategory() {
+    return category;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Menu m = (Menu)o;
+    return (this.getId() == m.getId());
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
+  }
+}

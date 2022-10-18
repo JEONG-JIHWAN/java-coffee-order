9c897eb9b34cf73b3af9de3bfc9145c5c0add607
JEONG-JIWHAN <jjw981210@gmail.com>
2022-10-19 04:32:16 +0900

    feat: 메뉴 정렬 기능 추가

--- src/main/java/coffee/order/Repository/MenuRepository.java
+++ src/main/java/coffee/order/Repository/MenuRepository.java
@@ -1,13 +1,19 @@
 package coffee.order.Repository;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import coffee.order.Domain.Menu;
 
 public class MenuRepository {
   private final Set<Menu> menus = new HashSet<>();
+
+  public MenuRepository() {
+  }
 
   public void save(Menu menu){
     menus.add(menu);
@@ -19,4 +25,15 @@ public class MenuRepository {
                 .findFirst()
                 .orElseThrow(()-> new IllegalStateException("해당 메뉴를 찾을 수 없습니다."));
   }
+
+  public List<Menu> showMenu(){
+    return sortMenu().stream().collect(Collectors.toList());
+  }
+
+  private List<Menu> sortMenu() {
+    List<Menu> menuList = new ArrayList<>(menus);
+    menuList.sort(Comparator.comparing((menu -> menu.getId())));
+    return menuList.stream().collect(Collectors.toList());
+  }
+
 }

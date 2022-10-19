2468e83efd9fa3c3603213a476961e0cc74e2762
JEONG-JIWHAN <jjw981210@gmail.com>
2022-10-19 14:18:08 +0900

    feat: 쿠폰 추가, 사용 기능 추가

---
+++ src/main/java/coffee/order/Domain/Coupon.java
@@ -0,0 +1,24 @@
+package coffee.order.Domain;
+
+public class Coupon {
+  private static final int TEN = 10;
+  private int count;
+
+  public Coupon(int count) {
+    this.count = count;
+  }
+
+  public void addCoupon(int orderCount){
+    this.count += orderCount;
+  }
+
+  public boolean checkUseCoupon(){
+    return this.count>=10;
+  }
+
+  public void minusCoupon(){
+    if(checkUseCoupon()){
+      this.count -= TEN;
+    }
+  }
+}
